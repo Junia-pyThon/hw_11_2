1248474a0e87b877175b2f1c17eee5762aa5b677
Evgenii <evrenbi4@gmail.com>
2022-07-29 22:18:48 +0300

    complite step 2 and add step 3

--- utils.py
+++ utils.py
@@ -1,73 +1,49 @@
 import json
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
+from setings import PATCH_CANDIDATES_JSON
 
 
-def load_candidates_from_json(path):
+def load_candidates_from_json() -> list[dict]:
     """
     Загружаем список кандидатов и информацию о них из файла
     :return:
     """
-    with open(path, encoding='utf-8') as file:
-        candidates = json.load(file)
-        return candidates
+    with open(PATCH_CANDIDATES_JSON, encoding='utf-8') as file:
+        return json.load(file)
 
 
-def get_candidate(candidate_id: str, candidates):
+def get_candidate(candidate_id: int) ->dict:
     """
     На странице /candidates/<pk> выводим информацию о кандидате по его id
     :return:
     """
-    for candidate in candidates:
-        if candidate['id'] == int(candidate_id):
-            return f"""
-                    <pre>
-                    <img src="({candidate['picture']})">
-                    Имя кандидата: {candidate['name']}
-                    Позиция кандидата: {candidate['position']}
-                    Навыки кандидата: {candidate['skills'].lower()}
-                    </pre>
-                    """
+    for candidate in load_candidates_from_json():
+        if candidate['id'] == candidate_id:
+            return candidate
 
 
-def get_candidates_by_name(candidate_name, candidates ):
-    for candidate in candidates:
+def get_candidates_by_name(candidate_name: str) -> list[dict]:
+    result = []
+    for candidate in load_candidates_from_json():
         if candidate['name'] == int(candidate_name):
-            return f"""
-                    <pre>
-                    <img src="({candidate['picture']})">
-                    Имя кандидата: {candidate['name']}
-                    Пол: {candidate['gender']}
-                    Возраст: {candidate['age']}
-                    Позиция кандидата: {candidate['position']}
-                    Навыки кандидата: {candidate['skills'].lower()}
-                    </pre>
-                    """
+            result.append(candidate)
+    return result
 
 
-def get_candidates_by_skill(skill_name, candidates):
-    result = ''
-    for candidate in candidates:
-        if skill_name in candidate['skills']:
-            result += f"""
-                        <pre>
-                        Имя кандидата: {candidate['name']}
-                        Позиция кандидата: {candidate['position']}
-                        Навыки кандидата: {candidate['skills'].lower()}
-                        </pre>
-                        \n\t
-                        """
+def get_candidates_by_skill(skill_name: str) -> list[dict]:
+    result = []
+    for candidate in load_candidates_from_json():
+        if skill_name in candidate['skills'].lower().split(', '):
+            result.append(candidate)
     return result
 
 
 @app.route('/')
-def get_all(candidates):
+def get_all(candidates=load_candidates_from_json()):
     """
      На главной странице выводим список всех кандитатов
     :return:
     """
     return render_template('list.html', candidates=candidates)
 
-app.run()
+
 
